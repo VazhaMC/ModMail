481e059333a7ed6b8d355c1a7362326e48440344
Hel0 <153912966+VazhaMC@users.noreply.github.com>
2024-03-30 01:29:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
         id: '1223350694006751272' // ← Your bot ID
     },
     modmail: {
-        guildId: '1198664775064883341 ', // ← Your server ID
+        guildId: '1198664775064883341a ', // ← Your server ID
         categoryId: '1223352842916597930', // ← The modmail category ID
         staffRoles: ['1223353104708276294'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
