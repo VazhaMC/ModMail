23f17fbb27b8d5dcc97833e93a0225ee5277fd34
Hel0 <153912966+VazhaMC@users.noreply.github.com>
2024-03-30 01:00:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
     client: {
-        token: 'put ur discord bot token here', // ← Your bot token (.env IS RECOMMENDED)
-        id: 'put ur damn discord bot client id here' // ← Your bot ID
+        token: 'MTIyMzM1MDY5NDAwNjc1MTI3Mg.GJoMq_.yXUOS3kNUSZM6l_T1_p2XMHwp3F6B4uwLS_z9A', // ← Your bot token (.env IS RECOMMENDED)
+        id: '1223350694006751272' // ← Your bot ID
     },
     modmail: {
-        guildId: 'your discord server id ', // ← Your server ID
-        categoryId: 'put ur category ID here', // ← The modmail category ID
-        staffRoles: ['Put the admin/staff role id here'], // ← The modmail staff roles IDs
+        guildId: '1198664775064883341 ', // ← Your server ID
+        categoryId: '1223352842916597930', // ← The modmail category ID
+        staffRoles: ['1223353104708276294'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
-        webhookURL: 'put ur foking discord channel webhook link here' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+        webhookURL: 'https://discord.com/api/webhooks/1223351173512171592/_lqEJh_a3_XINDca9ZCt6q3Manfy5fcIRD7Rsoh5gbBaRjYAkFmKqPKq3xxHhpn4LCRh' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
     }
 };
