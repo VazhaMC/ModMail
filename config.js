9079fc074453be54d324d15547f0aba0c9d8f116
Hel0 <153912966+VazhaMC@users.noreply.github.com>
2024-03-30 01:24:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     client: {
-        token: 'MTIyMzM1MDY5NDAwNjc1MTI3Mg.GEUbZ_.WHZb1kqenJxvzwD33ZcB5BPOY0fWvCfSgJxgOE', // ← Your bot token (.env IS RECOMMENDED)
+        token: 'MTIyMzM1MDY5NDAwNjc1MTI3Mg.GGUZTa.6QjPR5mV0XhsHMAljiRp2HP83O8utytCJlFYsI', // ← Your bot token (.env IS RECOMMENDED)
         id: '1223350694006751272' // ← Your bot ID
     },
     modmail: {
